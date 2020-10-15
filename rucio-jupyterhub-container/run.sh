9a9c9d5ed86fa404190facd3211bbed83cc983d3
Rob Barnsley <robbarnsley@users.noreply.github.com>
2020-10-15 10:51:53 +0100

    Add jhub container (#16)
    * adding jhub extension container
    
    * amending container name
    
    Co-authored-by: Rob Barnsley <R.Barnsley@skatelescope.org>

---
+++ rucio-jupyterhub-container/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --env-file env --rm -p 8888:8888 -e RUCIO_CFG_ACCOUNT=robbarnsley -v /home/eng/.globus/client.crt:/home/jovyan/client.crt -v /home/eng/.globus/client.key:/home/jovyan/client.key --name=rucio-jhub rucio-jhub:latest
